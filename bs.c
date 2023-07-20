5091385aff6cc95f8bfd365987bd74ee008d3ff0
LevanNamicheishvili <levannnamicheishvili@gmail.com>
2023-07-20 03:28:20 -0700

    feat: Implement account-related functions and data structure
    This commit adds the necessary code to handle accounts and their related operations. It introduces the MAX_NAME_LENGTH constant to limit the size of the account holder's name. The Account struct is defined with accountNumber, holderName, and balance fields.
    
    Additionally, the following functions are implemented:
    - createAccount: Creates a new account with the given account number and holder name.
    - deposit: Allows deposits of a specified amount into the account.
    - withdraw: Allows withdrawals of a specified amount from the account.
    - displayAccount: Displays the account details, including the account number, holder name, and balance.
    
    These changes provide the foundation for managing accounts in the system.

--- bs.c
+++ bs.c
@@ -2,3 +2,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+
+#define MAX_NAME_LENGTH 100 
+
+struct  Account
+{
+    char accountNumber[20];
+    char holderName[MAX_NAME_LENGTH];
+    double balance;
+};
+
+struct  Account* createAccount(const char* accountNumber, const char* holderName);
+void deposit (struct Account* account, double amount );
