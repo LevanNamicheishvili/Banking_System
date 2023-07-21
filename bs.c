ed148cf2b1749fd77d1dfb0f527493104a34d4af
LevanNamicheishvili <levannnamicheishvili@gmail.com>
2023-07-21 01:28:50 -0700

    feat
    The function initializes a variable numAccounts to keep track of the number of created accounts and dynamically allocates memory for an array of Account pointers called accounts.
    
    A while loop is used to repeatedly display the main menu and prompt the user for their choice.
    
    The user can choose to create an account, log in, or exit the program. Based on the user's choice, the relevant functionality is executed.
    
    For account creation (case '1'), the user is prompted to enter an account number and an account holder name. The createAccount() function is called to create a new account with the provided details.
    
    If the account creation is successful, the new account is added to the accounts array using dynamic memory allocation to resize the array. The numAccounts variable is incremented accordingly.
    
    If the maximum number of accounts is reached, an error message is displayed, indicating that the user cannot create more accounts.

--- bs.c
+++ bs.c
@@ -14,3 +14,37 @@ struct  Account
 
 struct  Account* createAccount(const char* accountNumber, const char* holderName);
 void deposit (struct Account* account, double amount );
+void withdraw(struct Account* account, double amount);
+void displayAccount(const struct Account* account);
+
+int main()
+{ 
+    int numAccounts = 0;
+    struct Account** accounts = (struct Account**)malloc(0);
+
+    while(1) 
+    { 
+        printf("\n1. Create Account\n");
+        printf("2. Login \n");
+        printf("3. Exit\n");
+        printf("Enter Your Choice : ");
+
+        char option; 
+        scanf("%c, &option");
+
+        switch(option)
+        {
+            case '1' : 
+            { 
+                char accountNumber[20];
+                char holderName[MAX_NAME_LENGTH];
+
+                printf("Enter account number : ");
+                scanf("%s", accountNumber);
+
+                printf("Enter account holder number: "); 
+                getchar();
+            }
+        }
+    }
+}
